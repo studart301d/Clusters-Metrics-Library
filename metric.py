1ef6c9fd8d941cd7c4d0ce5a250d08f5fd177bae
studart301d <gabrielstudart@alu.ufc.br>
2018-07-30 17:29:44 -0300

    atualizado

--- metric.py
+++ metric.py
@@ -18,6 +18,16 @@ def silhouette_samples(X,labels,metric='euclidean',**kwds):
 def silhouette_score(X,labels,metric='euclidean',sample_size=None,random_state=None,**kwds):
     return unsupervised.silhouette_score(X,labels,metric,sample_size,random_state,**kwds)
 
-def silhouette_plot():
+def silhouette_plot(X,labels,metric='euclidean',**kwds):
     silhouette_samples = unsupervised.silhouette_samples(X,labels,metric,**kwds)
-    sns.countplot()
+    df = pd.DataFrame(silhouette_samples)
+    df['cluster'] = labels
+    cluster_means = df.groupby('cluster').mean()
+    dit = dict(zip(cluster_means.index,cluster_means[0]))
+    df2 = pd.DataFrame(list(dit.items()))
+    df2.columns = ['Cluster','silhouette_mean']
+    df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
+    fig = plt.figure(figsize=(12,24))
+    ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h',)
+    ax.set_yticklabels(df2['Cluster'])
+    plt.show()
