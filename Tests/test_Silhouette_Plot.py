5cb52556f0b656e74f230e931953d92c537628d1
Weslley Caldas <weslleylc@gmail.com>
2018-08-01 21:05:48 -0300

    Adcionando Path relativo

--- Tests/test_Silhouette_Plot.py
+++ Tests/test_Silhouette_Plot.py
@@ -1,14 +1,22 @@
 import numpy as np 
-import pandas as pd 
+import pandas as pd
+from sklearn.datasets import make_blobs 
 import matplotlib.pyplot as plt 
 import seaborn as sns
-
 import sys
-sys.path.insert(0,r'C:\Projetos\Machine_Learning\Cluster_Metrics\Clusters-Metrics-Library')
+from pathlib import Path
+import os
+
+
+#working with relative path's
+#get the currently worked direcotry
+directory=Path(os.getcwd()).parents[0]
+sys.path.insert(0,str(directory))
+
+
 
 import metric
 
-from sklearn.datasets import make_blobs
 
 data = make_blobs(n_samples=200, n_features=2, 
                            centers=4, cluster_std=1.8,random_state=101)
