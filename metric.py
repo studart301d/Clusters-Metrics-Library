822be54abd99f8a338d534dc11a2712224aa0aeb
studart301d <gabrielstudart@alu.ufc.br>
2018-08-06 16:18:19 -0300

    plot

--- metric.py
+++ metric.py
@@ -18,26 +18,51 @@ def silhouette_samples(X,labels,metric='euclidean',**kwds):
 def silhouette_score(X,labels,metric='euclidean',sample_size=None,random_state=None,**kwds):
     return unsupervised.silhouette_score(X,labels,metric,sample_size,random_state,**kwds)
 
-def silhouette_plot(X,labels,metric='euclidean',fig_size = None,**kwds):
+def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,cluster = None,**kwds):
     
     silhouette_samples = unsupervised.silhouette_samples(X,labels,metric,**kwds)
     
     df = pd.DataFrame(silhouette_samples)
     df['cluster'] = labels
-    cluster_means = df.groupby('cluster').mean()
-    dit = dict(zip(cluster_means.index,cluster_means[0]))
-    df2 = pd.DataFrame(list(dit.items()))
-    df2.columns = ['Cluster','silhouette_mean']
-    df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
-    
-    if(fig_size == None):
-        if len(df2) > 64:
-            fig = plt.figure(figsize=(len(df2)/8,len(df2)/4))
-        else:
-            fig = plt.figure(figsize=(8,6))
-    else:
-        fig = plt.figure(figsize = fig_size)
-        
-    ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h',)
-    ax.set_yticklabels(df2['Cluster'])
-    plt.show()
+
+    if type == 'None':
+
+	    cluster_means = df.groupby('cluster').mean()
+	    dit = dict(zip(cluster_means.index,cluster_means[0]))
+	    df2 = pd.DataFrame(list(dit.items()))
+	    df2.columns = ['Cluster','silhouette_mean']
+	    df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
+	    
+	    if(fig_size == None):
+	        if len(df2) > 64:
+	            fig = plt.figure(figsize=(len(df2)/8,len(df2)/4))
+	        else:
+	            fig = plt.figure(figsize=(8,6))
+	    else:
+	        fig = plt.figure(figsize = fig_size)
+	        
+	    ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h',)
+	    ax.set_yticklabels(df2['Cluster'])
+	    plt.show()
+
+	elif type == 'cluster':
+
+		df['id_cliente'] = labels['id_cliente']
+		cluster = df[df['cluster'] == cluster]
+		cluster.columns = ['silhouette','Cluster','id_cliente']
+		cluster = cluster.sort_values(['silhouette'],ascending=False).reset_index(drop=True)
+
+		if(fig_size == None):
+	        if len(cluster) > 64:
+	            fig = plt.figure(figsize=(len(cluster)/8,len(cluster)/4))
+	        else:
+	            fig = plt.figure(figsize=(8,6))
+	    else:
+	        fig = plt.figure(figsize = fig_size)
+	        
+	    ax = sns.barplot(cluster['silhouette'],y = cluster.index.values,orient='h',)
+	    ax.set_yticklabels(cluster['Clientes'])
+	    plt.show()
+
+
+
