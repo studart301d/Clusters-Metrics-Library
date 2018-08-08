4c403dad1d0f11804a2969de7a6bf442c5a40b8d
studart301d <gabrielstudart@alu.ufc.br>
2018-08-08 15:55:33 -0300

    plot

--- metric.py
+++ metric.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul 29 19:19:57 2018
-
 @author: gabri
 """
 
@@ -28,41 +27,40 @@ import dunn_sklearn
 from sklearn.metrics.cluster import unsupervised
 
 def silhouette_samples(X,labels,metric='euclidean',**kwds):
-    return unsupervised.silhouette_samples(X,labels,metric,**kwds)
+	return unsupervised.silhouette_samples(X,labels,metric,**kwds)
 
 def silhouette_score(X,labels,metric='euclidean',sample_size=None,random_state=None,**kwds):
-    return unsupervised.silhouette_score(X,labels,metric,sample_size,random_state,**kwds)
+	return unsupervised.silhouette_score(X,labels,metric,sample_size,random_state,**kwds)
 
 def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,cluster = None,y=None,**kwds):
     
-    silhouette_samples = unsupervised.silhouette_samples(X,labels,metric,**kwds)
-    
-    df = pd.DataFrame(silhouette_samples)
-    df['cluster'] = labels
+	silhouette_samples = unsupervised.silhouette_samples(X,labels,metric,**kwds)
 
-    if type == None:
+	df = pd.DataFrame(silhouette_samples)
+	df['cluster'] = labels
 
-	    cluster_means = df.groupby('cluster').mean()
-	    dit = dict(zip(cluster_means.index,cluster_means[0]))
-	    df2 = pd.DataFrame(list(dit.items()))
-	    df2.columns = ['Cluster','silhouette_mean']
-	    df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
-	    
-	    if fig_size == None:
-	        if len(df2) > 64:
-	            fig = plt.figure(figsize=(len(df2)/8,len(df2)/4))
-	        else:
-	            fig = plt.figure(figsize=(8,6))
-	    else:
-	        fig = plt.figure(figsize = fig_size)
-	        
-	    if y == None:
-	    	ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h')
-	    else:
-	    	ax = sns.barplot(df2['silhouette_mean'],y = y,orient='h')
+	if type == None:
+		cluster_means = df.groupby('cluster').mean()
+		dit = dict(zip(cluster_means.index,cluster_means[0]))
+		df2 = pd.DataFrame(list(dit.items()))
+		df2.columns = ['Cluster','silhouette_mean']
+		df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
+		
+		if fig_size == None:
+			if len(df2) > 64:
+				fig = plt.figure(figsize=(len(df2)/8,len(df2)/4))
+			else:
+				fig = plt.figure(figsize=(8,6))
+		else:
+			fig = plt.figure(figsize = fig_size)
 
-	    ax.set_yticklabels(df2['Cluster'])
-	    plt.show()
+		if y == None:
+			ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h')
+		else:
+			ax = sns.barplot(df2['silhouette_mean'],y = y,orient='h')
+
+		ax.set_yticklabels(df2['Cluster'])
+		plt.show()
 
 	elif type == 'cluster':
 
@@ -71,20 +69,20 @@ def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,clus
 		cluster = cluster.sort_values(['silhouette'],ascending=False).reset_index(drop=True)
 
 		if fig_size == None:
-	        if len(cluster) > 64:
-	            fig = plt.figure(figsize=(len(cluster)/8,len(cluster)/4))
-	        else:
-	            fig = plt.figure(figsize=(8,6))
-	    else:
-	        fig = plt.figure(figsize = fig_size)
-	        
-	    if y == None:
-	    	ax = sns.barplot(cluster['silhouette'],y = cluster.index.values,orient='h')
-	    else:
-	    	ax = sns.barplot(cluster['silhouette'],y = y,orient='h')
+			if len(cluster) > 64:
+				fig = plt.figure(figsize=(len(cluster)/8,len(cluster)/4))
+			else:
+				fig = plt.figure(figsize=(8,6))
+		else:
+		fig = plt.figure(figsize = fig_size)
 
-	    ax.set_yticklabels(cluster['Dados'])
-	    plt.show()
+		if y == None:
+			ax = sns.barplot(cluster['silhouette'],y = cluster.index.values,orient='h')
+		else:
+			ax = sns.barplot(cluster['silhouette'],y = y,orient='h')
+
+		ax.set_yticklabels(cluster['Dados'])
+		plt.show()
 
 
 #Implementation of Dunn index
@@ -102,5 +100,3 @@ def min_cluster_distances(X, distances):
 
 def dunn_index(X, distances):
 	return dunn_sklearn.dunn(labels, euclidean_distances(X.drop('labels')))
-
-
