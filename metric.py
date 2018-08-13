a719ced74d14a85197846d41c54c33f4866ac959
studart301d <gabrielstudart@alu.ufc.br>
2018-08-13 15:22:09 -0300

    silhouette_plot of cluster finished

--- metric.py
+++ metric.py
@@ -17,7 +17,7 @@ import sys
 directory=Path(os.getcwd()).parents[0]
 sys.path.insert(0,str(directory))
 
-import dunn_sklearn
+#import dunn_sklearn
 
 
 
@@ -32,7 +32,7 @@ def silhouette_samples(X,labels,metric='euclidean',**kwds):
 def silhouette_score(X,labels,metric='euclidean',sample_size=None,random_state=None,**kwds):
 	return unsupervised.silhouette_score(X,labels,metric,sample_size,random_state,**kwds)
 
-def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,cluster = None,y=None,**kwds):
+def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,cluster = None,y=None,index = False,**kwds):
     
 	silhouette_samples = unsupervised.silhouette_samples(X,labels,metric,**kwds)
 
@@ -44,7 +44,7 @@ def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,clus
 		dit = dict(zip(cluster_means.index,cluster_means[0]))
 		df2 = pd.DataFrame(list(dit.items()))
 		df2.columns = ['Cluster','silhouette_mean']
-		df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
+		
 		
 		if fig_size == None:
 			if len(df2) > 64:
@@ -54,20 +54,22 @@ def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,clus
 		else:
 			fig = plt.figure(figsize = fig_size)
 
-		if y == None:
-			ax = sns.barplot(df2['silhouette_mean'],y = cluster_means.index,orient='h')
-		else:
-			ax = sns.barplot(df2['silhouette_mean'],y = y,orient='h')
 
+		df2 = df2.sort_values(['silhouette_mean'],ascending=False).reset_index(drop=True)
+		ax = sns.barplot(df2['silhouette_mean'],y = df2.index,orient='h')
 		ax.set_yticklabels(df2['Cluster'])
-		plt.show()
+		plt.ylabel('Cluster')
+		plt.show()	
 
 	elif type == 'cluster':
 
-		cluster = df[df['cluster'] == cluster]
-		cluster.columns = ['silhouette','Cluster']
-		cluster = cluster.sort_values(['silhouette'],ascending=False).reset_index(drop=True)
+		if index == True:
+			df['y'] = y
 
+		cluster = df[df['cluster'] == cluster]
+		cluster.columns = ['silhouette','Cluster','y']
+		cluster = cluster.sort_values(['silhouette'],ascending=False).reset_index(drop=True)
+		
 		if fig_size == None:
 			if len(cluster) > 64:
 				fig = plt.figure(figsize=(len(cluster)/8,len(cluster)/4))
@@ -76,27 +78,30 @@ def silhouette_plot(X,labels,metric='euclidean',fig_size = None,type = None,clus
 		else:
 			fig = plt.figure(figsize = fig_size)
 
-		if y == None:
-			ax = sns.barplot(cluster['silhouette'],y = cluster.index.values,orient='h')
-		else:
-			ax = sns.barplot(cluster['silhouette'],y = y,orient='h')
+		ax = sns.barplot(cluster['silhouette'],y = cluster.index,orient='h')
 
-		ax.set_yticklabels(cluster['Dados'])
+		if index == True:
+			ax.set_yticklabels(cluster['y'])
+
+		plt.ylabel('Index')
 		plt.show()
+        
+        
+
 
 
 #Implementation of Dunn index
 
-from sklearn.metrics.pairwise import euclidean_distances
+# from sklearn.metrics.pairwise import euclidean_distances
 
-def normalize(X):
-	return dunn_sklearn.normalize_to_smallest_integers(X)
+# def normalize(X):
+# 	return dunn_sklearn.normalize_to_smallest_integers(X)
 
-def cluster_diameter(X, distances):
-	return dunn_sklearn.diameter(X, euclidean_distances(X.drop('labels')))
+# def cluster_diameter(X, distances):
+# 	return dunn_sklearn.diameter(X, euclidean_distances(X.drop('labels')))
 
-def min_cluster_distances(X, distances):
-	return dunn_sklearn.min_cluster_distances(X, euclidean_distances(X.drop('labels')))
+# def min_cluster_distances(X, distances):
+# 	return dunn_sklearn.min_cluster_distances(X, euclidean_distances(X.drop('labels')))
 
-def dunn_index(X, distances):
-	return dunn_sklearn.dunn(labels, euclidean_distances(X.drop('labels')))
+# def dunn_index(X, distances):
+# 	return dunn_sklearn.dunn(labels, euclidean_distances(X.drop('labels')))
